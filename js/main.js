2e3987f6a11d9d3750b271e62c98724e8f167c87
phucnguyen2208 <dinhphuc208@gmail.com>
2020-11-25 16:34:05 +0700

    update new

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // back to top
+    $(".backtop").click(function() {
+        $("html, body").animate({scrollTop: 0}, 1000);
+    }); 
+});
+
+// Menu
+document.querySelector('.btmenu').addEventListener('click', function () {
+    document.querySelector('.nav-menu').classList.toggle('active');
+    this.classList.toggle('clicked');
+});
